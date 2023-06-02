f79c4709f5c98cb51be639355878fa922a62662d
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-02 23:12:40 +0500

    Asdf

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// document.getElementById("enter").onclick = function (paras) {
-//     var password = document.getElementById('data').value;
-// console.log(password);
-// document.getElementById('output').innerHTML = password;
-//     if (password.length < 8 ) {
-//         alert("Enter eight digit code ")
-//         return;
-//     }
+document.getElementById("enter").onclick = function (paras) {
+    var password = document.getElementById('data').value;
+console.log(password);
+document.getElementById('output').innerHTML = password;
+    if (password.length < 8 ) {
+        alert("Enter eight digit code ")
+        return;
+    }
     
-// }
+}
 
