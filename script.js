1e0b276aeb3ec83071bbdc39c7c9b43dbd262016
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-03 22:41:02 +0500

    asdffdsa

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let students =  [
-    {name:'Naveed', dob : '10-11-1997', email: 'naveed@techloset.com'},
-    {name:'ali', dob : '10-11-1996', email: 'ali@techloset.com'},
-    {name:'zain', dob : '10-11-1995', email: 'zain@techloset.com'}
-]
+// let students =  [
+//     {name:'Naveed', dob : '10-11-1997', email: 'naveed@techloset.com'},
+//     {name:'ali', dob : '10-11-1996', email: 'ali@techloset.com'},
+//     {name:'zain', dob : '10-11-1995', email: 'zain@techloset.com'}
+// ]
 
-   const program = students.map((element)=>{
-    // console.log(element)
-    var date = element.dob
-    var cal = new Date(date)
-   var re = cal.getFullYear()
-   var crrentYear = new Date().getFullYear()
-   var age =  crrentYear - re
-    students.push(age)
-   return `my age is ${age}`
+//    const program = students.map((element)=>{
+//     // console.log(element)
+//     var date = element.dob
+//     var cal = new Date(date)
+//    var re = cal.getFullYear()
+//    var crrentYear = new Date().getFullYear()
+//    var age =  crrentYear - re
+//     students.push(age)
+//    return `my age is ${age}`
 
-   element.age 
-   // console.log(cal);
-   //    var get = cal.setDate(date)
-   //    console.log(get);
+//    element.age 
+//    // console.log(cal);
+//    //    var get = cal.setDate(date)
+//    //    console.log(get);
    
    
-})
-console.log(students.push(program))
+// })
+// console.log(students.push(program))
