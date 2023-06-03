8cadd75626e5d00da138c4774a63df8ee50dec49
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-03 19:48:34 +0500

    age function

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-// document.getElementById("enter").onclick = function (paras) {
-//     var password = document.getElementById('data').value;
-// console.log(password);
-// document.getElementById('output').innerHTML = password;
-//     if (password.length < 8 ) {
-//         alert("Enter eight digit code ")
-//         return;
-//     }
-    
-// }
+let students =  [
+    {name:'Naveed', dob : '10-11-1997', email: 'naveed@techloset.com'},
+    {name:'ali', dob : '10-11-1996', email: 'ali@techloset.com'},
+    {name:'zain', dob : '10-11-1995', email: 'zain@techloset.com'}
+]
 
+   const program = students.map((element)=>{
+    // console.log(element)
+    var date = element.dob
+    var cal = new Date(date)
+   var re = cal.getFullYear()
+   var crrentYear = new Date().getFullYear()
+   var age =  crrentYear - re
+    students.push(age)
+   return `my age is ${age}`
+
+   element.age 
+   // console.log(cal);
+   //    var get = cal.setDate(date)
+   //    console.log(get);
+   
+   
+})
+console.log(students.push(program))
